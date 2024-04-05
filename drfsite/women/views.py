67cddb8277244a4c1d35c83b888ca6420ca27bf2
Nikita Mikhailov <mikhailoffnikita2016@yandex.ru>
2024-04-05 20:10:22 +0300

    fourth lesson realization basic CRUD with the help class APIView

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,10 +1,8 @@
 from .models import Women
-from rest_framework import generics
 from .serializers import WomenSerializer
-
-from django.forms import model_to_dict
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 
 class WomenAPIView(APIView):
@@ -15,10 +13,29 @@ class WomenAPIView(APIView):
     def post(self, request):
         serializer = WomenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+    def put(self, request, *args, **kwargs):
+        pk = kwargs.get('pk', None)    
+        if not pk:
+            return Response({"error": "Method PUT not allowed"})
         
-        post_new = Women.objects.create(
-            title=request.data['title'],
-            content=request.data['content'],
-            cat_id=request.data['cat_id']
-        )  
-        return Response({'post': WomenSerializer(post_new).data})
+        try:
+            instance = Women.objects.get(pk=pk)
+        except:
+            return Response({"error": "Object does not exists"})    
+
+        serializer = WomenSerializer(data=request.data, instance=instance)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"post": serializer.data})
+    
+    def delete(self, request, pk):
+        try:
+            instance = Women.objects.get(pk=pk)
+        except Women.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        instance.delete()
+        return Response({"post": f"Women id: {pk} is deleted"})
+       
