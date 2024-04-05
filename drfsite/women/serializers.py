45228bd4f00619a962c8339ca8d82d5ac1774fc5
Nikita Mikhailov <mikhailoffnikita2016@yandex.ru>
2024-04-05 16:00:59 +0300

    third lesson create learn serializer

--- drfsite/women/serializers.py
+++ drfsite/women/serializers.py
@@ -1,7 +1,37 @@
-from rest_framework import serializers
 from .models import Women
+from rest_framework import serializers
 
-class WomenSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Women
-        fields = ('title', 'content', 'cat_id')
+
+class WomenSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    cat_id = serializers.IntegerField()
+
+
+# class WomenModel:
+#     def __init__(self, title, content):
+#         self.title = title
+#         self.content = content 
+
+
+# class WomenSerializer(serializers.Serializer):
+#     title = serializers.CharField(max_length=255)
+#     content = serializers.CharField()
+
+
+# def encode():
+#     model = WomenModel('Angelina Joli', 'Content: Angelina Joli') 
+#     model_sr = WomenSerializer(model) 
+#     print(model_sr.data, type(model_sr.data), sep='\n')   
+#     json = JSONRenderer().render(model_sr.data)
+#     print(json)
+
+# def decode():
+#     stream = io.BytesIO(b'{"title": "Angelina Joli", "content": "Content: Angelina Joli"}')
+#     data = JSONParser().parse(stream)
+#     serializer = WomenSerializer(data=data)
+#     serializer.is_valid()
+#     print(serializer.validated_data)
